5afa19f07c02817fcc4c160f3de5823d77e11090
Misael Cardoso Pereira <90993863+1Walke@users.noreply.github.com>
2025-07-20 19:36:50 -0300

    Add files via upload

---
+++ planos.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Planos - Acolhe Food</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="logoacolhe.png" type="image/x-icon">
+</head>
+<body>
+
+  <header>
+    <h1>Planos Acolhe Food</h1>
+    <p>Escolha o plano que melhor se adapta a você</p>
+  </header>
+
+  <nav>
+    <a href="index.php">Início</a>
+    <a href="index.php#sobre">Sobre</a>
+    <a href="index.php#dicas">Dicas</a>
+    <a href="index.php#contato">Contato</a>
+  </nav>
+
+  <section>
+    <h2>Plano Basic R$ 14,99</h2>
+    <ul <a href="index.php"</a>
+      <li><strong>Plano Básico:</strong> Dicas semanais e conteúdos gratuitos.</li>
+      <li><strong>Plano Premium:</strong> Acesso a conteúdos exclusivos, vídeos e suporte nutricional.</li>
+      <li><strong>Plano Familiar:</strong> Conteúdos adaptados para toda a família.</li>
+      <a href="index.php" class="botao"> Conferir</a>
+    </ul>
+  </section>
+    <section>
+    <h2>Plano Medium R$ 24,99</h2>
+    <ul>
+      <li><strong>Plano Básico:</strong> Dicas semanais e conteúdos gratuitos.</li>
+      <li><strong>Plano Premium:</strong> Acesso a conteúdos exclusivos, vídeos e suporte nutricional.</li>
+      <li><strong>Plano Familiar:</strong> Conteúdos adaptados para toda a família.</li>
+       <a href="index.php" class="botao"> Conferir</a>
+    </ul>
+  </section>
+  <section>
+    <h2>Plano Premium R$ 34,99</h2>
+    <ul>
+      <li><strong>Plano Básico:</strong> Dicas semanais e conteúdos gratuitos.</li>
+      <li><strong>Plano Premium:</strong> Acesso a conteúdos exclusivos, vídeos e suporte nutricional.</li>
+      <li><strong>Plano Familiar:</strong> Conteúdos adaptados para toda a família.</li>
+       <a href="index.php" class="botao"> Conferir</a>
+    </ul>
+  </section>
+
+  <footer>
+    <p>&copy; <?php echo date("Y"); ?> Acolhe Food. Todos os direitos reservados.</p>
+    <a href="https://www.instagram.com/acolhefood.cie?igsh=ajdoYzM3dW1sMHF1" target="_blank">
+      <img id="simboloInsta" src="simbolo_insta.png" alt="Instagram Acolhe Food">
+    </a>
+  </footer>
+
+</body>
+</html>
