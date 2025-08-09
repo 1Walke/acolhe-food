a8617c8fa92c9aa4cf5c1a081c8c214dd181fda6
Misael Cardoso Pereira <90993863+1Walke@users.noreply.github.com>
2025-08-09 00:51:59 -0300

    Add files via upload

---
+++ mandarEmail.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const crypto = require('crypto');
+
+let codigos = {};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'misaelcardoso778@gmail.com',
+    pass: 'doab aswf nwtp yttw'
+  }
+});
+
+
+
+app.post('/link-recuperacao', (req, res) => {
+  const email = req.body.email;
+  const usuario = req.body.usuario;
+  const idUser = req.body.id;
+  const token = crypto.randomBytes(16).toString('hex');
+  codigos[idUser] = token; // Armazena o token associado ao id do usuário
+  const mailOptions = {
+    from: 'Acolhe Food <misaelcardoso778@gmail.com>',
+    to: 'misaelcardoso775@gmail.com',
+    subject: 'Verificação de Email',
+    html: `Olá ${usuario},<br><br> Clique no link para recuperar sua senha: <br><a href="http://localhost/Project/esqueceuSenha/novaSenha.php?token=${token}&id=${idUser}">Recuperar senha</a>`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Erro ao enviar o email');
+    } else {
+      console.log('Email enviado:', info.response);
+      res.send('Email enviado com sucesso!');
+    }
+  });
+});
+
+app.post('/verificar-codigo', (req, res) => {
+  const { token, idUser } = req.body;
+
+  if(codigos[idUser] && codigos[idUser].toString() === token.toString()) {
+    delete codigos[idUser]; // Remove o código após a verificação
+    res.send({sucesso: true});
+  }
+  else{
+    res.send({sucesso: false});
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
---
+++ server.js
@@ -0,0 +1,58 @@
+// server.js
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const crypto = require('crypto');
+
+let codigos = {};
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'misaelcardoso778@gmail.com',
+    pass: 'doab aswf nwtp yttw'
+  }
+});
+
+app.post('/send-email', (req, res) => {
+  const email = req.body.email;
+  const codigo = Math.floor(100000 + Math.random() * 900000);
+  codigos[email] = codigo; // Armazena o código associado ao email
+  const mailOptions = {
+    from: 'Acolhe Food <misaelcardoso778@gmail.com>',
+    to: 'misael.p@aluno.senai.br',
+    subject: 'Verificação de Email',
+    text: `Codigo de verificação: ${codigo}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Erro ao enviar o email');
+    } else {
+      console.log('Email enviado:', info.response);
+      res.send('Email enviado com sucesso!');
+    }
+  });
+});
+
+app.post('/verificar-codigo', (req, res) => {
+  const {email, codigoDigitado} =  req.body;
+
+  if(codigos[email] && codigos[email].toString() === codigoDigitado.toString()){
+    delete codigos[email]; // Remove o código após a verificação
+    res.send({sucesso: true});
+  }
+  else{
+    res.send({sucesso: false});
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
+
